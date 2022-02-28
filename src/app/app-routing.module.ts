30bf86d08e1390e23f4f58012ab17d30c9b7f79e
JuanJoseCM85 <juanjosecm85@gmail.com>
2022-02-28 16:53:56 +0100

    Finalizado el routing y los componentees

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "app-home", component: HomeComponent,},
+  {path: "app-registro", component: RegistroComponent},
+  { path: "**", pathMatch: "full", redirectTo: "app-home" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
