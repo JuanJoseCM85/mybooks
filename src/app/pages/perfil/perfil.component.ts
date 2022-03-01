680936f9ccdc33a5af439cd1840b55bea00d205e
JuanJoseCM85 <juanjosecm85@gmail.com>
2022-03-01 21:04:31 +0100

    retoque

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -11,19 +11,24 @@ export class PerfilComponent implements OnInit {
   public miUsuario: Usuario;
 
   constructor() { 
-    this.miUsuario = new Usuario(1,"Juanjo","Cabrera Maldonado","email@gmail.com","../../../assets/imagenperfil.jpg","password");
+    this.miUsuario = new Usuario(1,"Juanjo","Cabrera Maldonado","email@gmail.com","../../../assets/avatar.jpg","password");
   }
 
   ngOnInit(): void {
   }
 
-  enviarDatos(inputNombre,inputApellidos,inputEmail){
+  enviarDatos(inputNombre,inputApellidos,inputEmail,inputUrl){
     //opcion1 del reto
     //console.log(this.miUsuario.nombre);
     //opcion2
     this.miUsuario.nombre = inputNombre.value;
     this.miUsuario.apellidos = inputApellidos.value;
     this.miUsuario.correo = inputEmail;
+    console.log(inputUrl.value);
+    
+    if(inputUrl.value != "")
+      this.miUsuario.url = inputUrl.value;
+
 
 
     
